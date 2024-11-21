e393d4888243fd776ba9d642635b2b62e931a5d0
Matheus Ti <matheusgarciajbs@gmail.com>
2024-11-21 12:52:47 -0300

    rota privada e autenticacao

--- routes/public.js
+++ routes/public.js
@@ -1,11 +1,13 @@
 import express from "express"
 import { PrismaClient } from '@prisma/client'
 import bcrypt from 'bcrypt'
-import token from 'jsonwebtoken'
+import jwt from 'jsonwebtoken'
 
 
 const prisma = new PrismaClient()
 const router = express.Router()
+
+const JWT_SECRET = process.env.JWT_SECRET
 
 // ROTA criar usuarios
 router.post('/cadastro', async (req, res) => {
@@ -32,10 +34,27 @@ router.post('/cadastro', async (req, res) => {
 //ROTA logar usuario
 router.post('/login', async (req, res) => {
   try {
+    //busca o usuario no banco de dados
     const userInfo = req.body
     const user = await prisma.user.findUnique({
       where: {email: userInfo.email},
     })
+    
+    //verifica se o usuario existe
+    if (!user) {
+      return res.status(404).json({message: "Usuario não encontrado"})
+    }
+    const isMatch = await bcrypt.compare(userInfo.password, user.password)
+
+    //verifica se a senha esta correta
+    if (!isMatch) {
+      return res.status(404).json({message: "Senha invalida!"})
+    }
+
+    //gerar o token jwt
+    const token = jwt.sign({id: user.id}, JWT_SECRET, {expiresIn:'7d'})
+
+    res.status(200).json(token)
 
   } catch (err) {
     res.status(500).json({message:'erro'}) //resposta para o front
--- src/server.js
+++ src/server.js
@@ -1,37 +1,22 @@
 import { PrismaClient } from '@prisma/client'
 import express from 'express'
 import publicRoutes from '../routes/public.js'
+import privateRoutes from '../routes/private.js'
 
-
-// import { hash, compare } from 'bcrypt'
-// import { sign } from 'jsonwebtoken'
-
-
+import auth from '../middleware/auth.js'
 
 const app = express()
-const prisma = new PrismaClient()
+
 app.use(express.json())
 
 // listar usuarios
-app.get('/get',  async (req, res) => {
-
-  const user = await prisma.user.findMany()
-
-  res.send(user)
-  res.status(200)
-})
 
 // criar usuarios atualizados
 app.use('/usuarios', publicRoutes)
+app.use('/',auth, privateRoutes )
 
 // editar usuario
-app.put('/put', (req, res) => {
-  
-})
 
 //deletar usuario
-app.delete('/delete', (req, res) => {
-
-})
 
 app.listen(3000)
