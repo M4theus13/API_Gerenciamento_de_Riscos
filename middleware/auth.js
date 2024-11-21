d9c7b60a28525cb167380db2b6d252f17904e6d7
Matheus Ti <matheusgarciajbs@gmail.com>
2024-11-21 13:03:08 -0300

    teste

---
+++ middleware/auth.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken'
+
+const JWT_SECRET = process.env.JWT_SECRET
+
+const auth = (req, res, next) => {//next é a aprovação para continuar a aplicação
+
+  const token = req.headers.authorization
+
+
+   if (!token) {
+     return res.status(401).json({message: 'acesso negado'})
+   }
+
+  try {
+
+    const decodedToken = jwt.verify(token.replace('Bearer ', ''), JWT_SECRET)
+    console.log(decodedToken)  
+
+  } catch (err) {
+    return res.status(401).json({message: 'Token invalido'})
+  }
+
+  next()
+}
+
+export default auth
---
+++ routes/private.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const router = express.Router()
+const prisma = new PrismaClient()
+
+router.get('/listar', async (req, res) => {
+  try {
+    const user = await prisma.user.findMany() //esconde as informações (esconde o password)
+    res.status(200).json({message: 'usuarios listados', user})
+
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({message:'falha no servidor'}) //resposta para o front
+  }
+})
+
+export default router
