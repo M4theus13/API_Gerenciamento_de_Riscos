75b41488b797b61aa7d84aac1b136b048d8078d7
Matheus Trabalho <matheusgarciajbs@gmail.com>
2025-02-12 14:40:35 -0300

    autenticador envia informacao de adm

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,15 +7,13 @@ const auth = (req, res, next) => {//next é a aprovação para continuar a aplic
   const token = req.headers.authorization
 
    if (!token) {
-     return res.status(401).json({message: 'acesso negado'})
+    return res.status(401).json({message: 'acesso negado', admin})
    }
-
+   
   try {
-
     const decodedToken = jwt.verify(token.replace('Bearer ', ''), JWT_SECRET)
     req.userId = decodedToken.id //envia o user id para a req
-
-
+    req.isAdmin = decodedToken.admin //envia o admin para
   } catch (err) {
     return res.status(401).json({message: 'Token invalido'})
   }
