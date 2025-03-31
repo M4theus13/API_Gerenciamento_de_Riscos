388a5cb2de9eca2ce5da07e465d5bc2e7ba3afad
Matheus Trabalho <matheusgarciajbs@gmail.com>
2025-03-31 13:45:29 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -9,13 +9,19 @@ import auth from '../middleware/auth.js'
 const app = express()
 
 app.use(express.json())
-app.use(cors({ origin: 'https://m4theus13.github.io' }))
+
+app.use(cors({
+  origin: 'https://m4theus13.github.io', // Domínio base (sem o caminho do repositório)
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true // Se usar cookies ou tokens
+}));
 
 // criar usuarios atualizados
 app.use('/usuarios', publicRoutes)
 app.use('/admin', auth , adminRoutes)
 // listar usuarios
-app.use('/',auth, privateRoutes )
+app.use('/private',auth, privateRoutes )
 
 // editar usuario
 
