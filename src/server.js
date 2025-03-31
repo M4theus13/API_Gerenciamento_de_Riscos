b914b86deb26fd39d4548838d039211461ab24c2
Matheus Trabalho <matheusgarciajbs@gmail.com>
2025-03-31 17:30:39 -0300

    config

--- src/server.js
+++ src/server.js
@@ -3,15 +3,24 @@ import cors from 'cors'
 import publicRoutes from '../routes/public.js'
 import privateRoutes from '../routes/private.js'
 import adminRoutes from '../routes/admin.js' 
+import dotenv from 'dotenv';
 
 import auth from '../middleware/auth.js'
 
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+console.log("Ambiente:", process.env.NODE_ENV);
 const app = express()
+
+
 
 app.use(express.json())
 
+const allowedOrigins = process.env.NODE_ENV === 'production'
+  ? ['https://m4theus13.github.io']
+  : ['https://m4theus13.github.io', 'http://localhost:3000'];
+
 app.use(cors({
-  origin: 'https://m4theus13.github.io', // Domínio base (sem o caminho do repositório)
+  origin: allowedOrigins,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true // Se usar cookies ou tokens
@@ -21,15 +30,13 @@ app.get('/', (req, res) => {
   res.send('API funcionando! 🚀');
 });
 
-// criar usuarios atualizados
 app.use('/usuarios', publicRoutes)
 app.use('/admin', auth , adminRoutes)
-// listar usuarios
 app.use('/private',auth, privateRoutes )
 
-// editar usuario
 
-//deletar usuario
+console.log(process.env.NODE_ENV)
+
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
