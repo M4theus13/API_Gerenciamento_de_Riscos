cb5afbff5c29e88bd6b9ad58660e0a9c339b31c6
Matheus Trabalho <matheusgarciajbs@gmail.com>
2025-03-31 13:33:36 -0300

    port

--- src/server.js
+++ src/server.js
@@ -23,5 +23,5 @@ app.use('/',auth, privateRoutes )
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Servidor rodando em ${port}`);
 });
